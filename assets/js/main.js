46a00934fcab17da64d7bd776cd96c5146597e05
Anuj Singh <anujjadon98@gmail.com>
2019-07-26 16:41:55 +0530

    Add Contact form.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,11 @@
-
-
-
 $(document).ready(function() {
-
+   
     $('html').addClass('js-enabled');
 
     setup_nivo_lightbox();
     setup_dense();
 
-    $(window).load(function() {
+    $(window).on('load', function() {
         $(".js-preloader").fadeOut(800, function() {
             $(".js-main-container").fadeIn(800);
 
